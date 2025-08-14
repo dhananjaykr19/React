9ba2c87895ee2252ac51c218d33a3cee61d94d49
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-08-14 13:10:56 +0530

    feat: update RestaurantMenu component to utilize useParams and useRestaurantMenu hooks for data fetching

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import ItemList from './ItemList.jsx';
 import ItemDescription from './ItemDescription.jsx';
+import RestaurantCateogry from './RestaurantCateogry.jsx';
+import { useParams } from 'react-router-dom';
+import useRestaurantMenu from '../Hooks/useRestaurantMenu.js';
+import Shimmer from './shimmar';
 
 const RestaurantMenu = () => {
-    // const rawItemCards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
+    const {resId } = useParams();
+    const resInfo = useRestaurantMenu(resId);
+    
+    if (!resInfo) 
+        return <Shimmer />;
+        
+    const restaurantInfo = resInfo?.cards[2]?.card?.card?.info;
+    const rawItemCards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
 
     return (
         <div className="mx-auto text-center mt-10">
-            <ItemDescription />
-            <ItemList />
+            <ItemDescription resData={restaurantInfo}/>
+            <RestaurantCateogry resData={rawItemCards}/>
         </div>
     );
 };
