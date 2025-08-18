e53a2305983306f3fa447fa53b216a780d85dff6
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-08-18 14:27:59 +0530

    feat: implement cart slice with add, remove, and clear item reducers

---
+++ src/utils/cartSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const cartSlice = createSlice({
+    name : 'cart',
+    initialState : {
+        // items : ["burger", "pizza"]
+        items : []
+    },
+    reducers : {
+        addItem : (state, action) => {
+            // mutating the state here
+            state.items.push(action.payload);
+        },
+        removeItem : (state, action) => {
+            // TODO : check which index element have to remove
+            state.items.pop();
+        },
+        clearCart : (state) => {
+            state.items.length = 0;
+        }
+    }
+});
+
+export const {
+    addItem,
+    removeItem,
+    clearCart
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
